73dd62c9747db94731ce8e1e6274a98b737b2d00
Alina Kornilova <alinakornilova78@gmail.com>
2020-02-08 02:28:59 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,4 @@
 class Artist < ActiveRecord::Base
   has_many :songs 
   
-  def songs_ids=(ids)
-     ids.each do |id|
-       song = Song.find(id)
-       self.songs << song
-     end
-   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,19 +1,13 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist 
-  belongs_to :genre 
+  belongs_to :artist
+  belongs_to :genre
   has_many :notes
-  
-  accepts_nested_attributes_for :notes
-  
-  
-  #for Artist
+
   def artist_name=(name)
-     self.artist = Artist.find_or_create_by(name: name)
-   end
- 
-   def artist_name
-      self.artist ? self.artist.name : nil
-   end
-   
-  
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
 end
